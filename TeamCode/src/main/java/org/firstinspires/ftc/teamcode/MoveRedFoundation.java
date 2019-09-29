7844dd68ffdcb5d60c2910cec50721a7f7b89f4b
metalworks <github@ftc.rocks>
2019-09-29 15:56:03 -0400

    Moves red foundation to red building zone.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveRedFoundation.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "MoveRedFoundation")
+//@Disabled
+public class MoveRedFoundation extends MethodLibrary {
+
+    @Override
+    public void runOpMode() {
+
+        hardwareMap();
+
+        waitForStart();
+        //Put your autonomous code after this line
+
+
+        double driveSpeed=0.5;
+        double robotLength=17.5;
+        double dragSpeed=0.8;
+
+        //drive to foundation
+        drive(24,driveSpeed);
+        absoluteTurn(-90);
+        drive(29,driveSpeed);
+        absoluteTurn(0);
+        drive(28.5-robotLength,driveSpeed);
+        //attach to foundation
+        disengageHook();
+        drive(.5,driveSpeed);
+        engageHook();
+        //drive to building zone
+        reverse(59.5-robotLength,dragSpeed);
+        //detach foundation
+        disengageHook();
+        reverse(.5,driveSpeed);
+        engageHook();
+        //parking
+        absoluteTurn(-90);
+        reverse(52,driveSpeed);
+
+
+    }
+
+}
+
