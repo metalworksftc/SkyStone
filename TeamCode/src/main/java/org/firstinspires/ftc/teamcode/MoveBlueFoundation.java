2b0c62f4fe4a7c48272c450f605ed5edd5af05b3
metalworks <github@ftc.rocks>
2019-10-06 13:58:58 -0400

    Good settings for the automonous foundation blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveBlueFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveBlueFoundation.java
@@ -20,24 +20,25 @@ public class MoveBlueFoundation extends MethodLibrary {
         double dragSpeed=0.8;
 
         //drive to foundation
+        engageHook();
         drive(24,driveSpeed);
         absoluteTurn(90);
-        drive(29,driveSpeed);
+        drive(31,driveSpeed);
         absoluteTurn(0);
-        drive(28.5-robotLength,driveSpeed);
+        drive(26-robotLength,driveSpeed);
         //attach to foundation
         disengageHook();
-        drive(.5,driveSpeed);
+        drive(3.5,driveSpeed);
         engageHook();
         //drive to building zone
-        reverse(57.5-robotLength,dragSpeed);
+        reverse(58-robotLength,dragSpeed);
         //detach foundation
         disengageHook();
         reverse(.5,driveSpeed);
         engageHook();
         //parking
         absoluteTurn(90);
-        reverse(52,driveSpeed);
+        reverse(53,driveSpeed);
 
 
     }
