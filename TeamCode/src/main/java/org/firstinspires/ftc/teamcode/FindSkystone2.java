1404a1a2ca99e850385e7a2c38f917d22956f82c
metalworksftc <github@ftc.rocks>
2019-10-17 20:24:35 -0400

    back up to skystone position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindSkystone2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindSkystone2.java
@@ -24,21 +24,48 @@ public class FindSkystone2 extends MethodLibrary {
         int skystone = -1;
         if (isBlack(block1)) {
             skystone = 1;
+            goToStone1();
         }
         else if (isBlack(block3)) {
             skystone = 3;
+            goToStone3();
         }
         else {
             skystone = 2;
+            goToStone2();
         }
 
 
         while (true) {
-            telemetry.addLine("skystone" + " is" + (skystone));
+            telemetry.addLine("skystone" + " is " + (skystone));
             telemetry.update();
             cs.alpha();
         }
 
+    }
+
+    private void goToStone3() {
+        reverse(3,0.5);
+        absoluteTurn(90);
+        drive(3,0.5);
+        absoluteTurn(180);
+        reverse(3,0.5);
+    }
+
+    private void goToStone2() {
+        reverse(3,0.5);
+        absoluteTurn(-90);
+        drive(1.5,0.5);
+        absoluteTurn(180);
+        reverse(3,0.5);
+    }
+
+    private void goToStone1() {
+        reverse(3,0.5);
+        absoluteTurn(-90);
+        drive(10,0.5);
+        absoluteTurn(180);
+        reverse(3,0.5);
     }
 
     private boolean isBlack(int alpha) {
