a9870c495b0907f0baefb2845131667026c97fa7
metalworksftc <github@ftc.rocks>
2019-11-21 20:30:58 -0500

    Added code for strafe motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class TankDrive extends OpMode {
     Servo tailServo, stoneServo;
 
-    DcMotor lm, rm, vertical, horizontal;
+    DcMotor lm, rm, vertical, horizontal, strafeMotor;
 
 
     @Override
@@ -24,21 +24,39 @@ public class TankDrive extends OpMode {
         stoneServo = hardwareMap.servo.get("ss");
         vertical =  hardwareMap.dcMotor.get("tm");
         horizontal = hardwareMap.dcMotor.get("bm");
+        strafeMotor = hardwareMap.dcMotor.get("sm");
     }
 
     @Override
     public void loop() {
         rm.setPower(- gamepad1.left_stick_y);
         lm.setPower(- gamepad1.right_stick_y);
-        tailServo.setPosition(gamepad1.right_trigger * 0.4 + 0.6);
+        // TODO
+        //What if both triggers are pressed?
+
+        strafeMotor.setPower(gamepad1.right_trigger);
+        strafeMotor.setPower( - gamepad1.left_trigger);
+
+//        Slownessing D pad controls, salt is good
+        strafeMotor.setPower(decodeDPad(gamepad1.dpad_right));
+        strafeMotor.setPower( - decodeDPad(gamepad1.dpad_left));
+        lm.setPower(decodeDPad(gamepad1.dpad_up));
+        rm.setPower(decodeDPad(gamepad1.dpad_up));
+        lm.setPower( - decodeDPad(gamepad1.dpad_down));
+        rm.setPower( - decodeDPad(gamepad1.dpad_down));
+
+        tailServo.setPosition(gamepad2.left_trigger * 0.4 + 0.6);
         stoneServo.setPosition(gamepad2.right_trigger * -0.2 + 1);
         vertical.setPower( - gamepad2.left_stick_y);
         horizontal.setPower(gamepad2.right_stick_y);
 
 
-
-
     }
 
-
+    protected double decodeDPad(boolean button) {
+        if (button) {
+            return 0.5;
+        }
+        return 0;
+    }
 }
